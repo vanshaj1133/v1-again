469fb41bb0fca3856f0b8b5aea07bae3507eacc7
RAHUL DADWAL <rahulxdadwal@gmail.com>
2024-10-28 22:20:09 +0530

    changes in muklol's code

--- mukul first app/lib/main.dart
+++ mukul first app/lib/main.dart
@@ -2,31 +2,30 @@ import 'package:first/dashboard.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const mlsc());
+  runApp(mlsc());
 }
 
 class mlsc extends StatelessWidget {
-  const mlsc({super.key});
+  
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(primaryColor: Colors.black),
-      home: const homepage(),
+      home:  homepage(),
     );
   }
 }
 
 class homepage extends StatelessWidget {
-  const homepage({super.key});
-
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: ElevatedButton(onPressed: (){
-          Navigator.push(context, MaterialPageRoute(builder: (context) => const Dashboard()));
-        }, child: const Text("Navigate")),
+          Navigator.push(context, MaterialPageRoute(builder: (context) =>  Dashboard()));
+        }, child:  Text("Navigate")),
       ),
     );
   }
